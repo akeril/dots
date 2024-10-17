96ccd2e8dbed9bd8370d47826a3634f4fe0a567d
akeril <185402721+akeril@users.noreply.github.com>
2024-10-17 22:19:09 +0530

    dots

---
+++ modules/git/default.nix
@@ -0,0 +1,18 @@
+{ pkgs, lib, config, ... }:
+{
+  home.packages = with pkgs; [
+    git
+  ];
+
+  programs = {
+    git = {
+      enable = true;
+      userEmail = "185402721+akeril@users.noreply.github.com";
+      userName = "akeril";
+      extraConfig = {
+        init.defaultBranch = "main";
+        credential.helper = "store";
+      };
+    };
+  };
+}
