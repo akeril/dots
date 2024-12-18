c5ac02c8522029aa4f20aac0cee214a8eea6bc89
akeril <185402721+akeril@users.noreply.github.com>
2024-12-19 01:24:19 +0530

    nix: enable experimental commands

--- host/nix.nix
+++ host/nix.nix
@@ -1,6 +1,10 @@
 { pkgs, ... }:
 {
   nix = {
+    settings = {
+      experimental-features = [ "nix-command" "flakes" ];
+      use-xdg-base-directories = true;
+    };
     gc = {
       automatic = true;
       dates = "monthly";
