187d28fa99ed30a06467d378f4df6131dccc9599
akeril <185402721+akeril@users.noreply.github.com>
2025-02-03 18:07:59 +0530

    enable sidebar

--- modules/firefox/settings.nix
+++ modules/firefox/settings.nix
@@ -1,4 +1,6 @@
 {
   "layout.css.prefer-color-scheme.content-override" = true;
   "extensions.activeThemeID" = "firefox-compact-dark@mozilla.org";
+  "sidebar.revamp" = true;
+  "sidebar.verticalTabs" = true;
 }
