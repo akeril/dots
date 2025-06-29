0c2a22726c62e4072bde69f43625d2f199493ef7
akeril <185402721+akeril@users.noreply.github.com>
2025-06-29 15:40:20 +0530

    nvim: remove indent plugin

--- modules/nvim/lua/plugins/oil.lua
+++ modules/nvim/lua/plugins/oil.lua
@@ -1,9 +1,9 @@
 return {
 	"stevearc/oil.nvim",
 	dependencies = { "nvim-tree/nvim-web-devicons" },
-	init = function()
-		vim.keymap.set("n", "<leader>o", "<CMD>Oil --float<CR>", {})
-	end,
+	keys = {
+		{ "<leader>o", "<CMD>Oil --float<CR>" },
+	},
 	opts = {
 		skip_confirm_for_simple_edits = true,
 		keymaps = {
--- modules/nvim/lua/plugins/ui.lua
+++ modules/nvim/lua/plugins/ui.lua
@@ -1,17 +1,16 @@
 return {
-  {
-    "catppuccin/nvim",
-    name = "catppuccin",
-    priority = 1000,
-    config = function()
-      vim.cmd.colorscheme("catppuccin")
-    end,
-  },
-  {
-    "folke/snacks.nvim",
-    opts = {
-      indent = {},
-      input = {},
-    },
-  },
+	{
+		"catppuccin/nvim",
+		name = "catppuccin",
+		priority = 1000,
+		config = function()
+			vim.cmd.colorscheme("catppuccin")
+		end,
+	},
+	{
+		"folke/snacks.nvim",
+		opts = {
+			input = {},
+		},
+	},
 }
